60d594cea213fd1e10cffe6a42fdac4af5f8e734
Robert Young <thegreekgeekseter@gmail.com>
2025-06-24 23:02:56 +0000

    Added new section, collection.

--- .eleventy.js
+++ .eleventy.js
@@ -124,6 +124,28 @@ module.exports = async function (eleventyConfig) {
     return coll;
   });
 
+  eleventyConfig.addCollection("projects", function (collection) {
+    /* The projects collection includes all slash pages that list 'projects' in the front matter 'tags'
+       - https://www.11ty.dev/docs/collections/
+    */
+
+    // EDIT HERE WITH THE CODE FROM THE NEXT STEPS PAGE TO REVERSE CHRONOLOGICAL ORDER
+    // (inspired by https://github.com/11ty/eleventy/issues/898#issuecomment-581738415)
+    const coll = collection.getFilteredByTag("projects");
+
+    // From: https://github.com/11ty/eleventy/issues/529#issuecomment-568257426
+    // Adds {{ prevSlash.url }} {{ prevSlash.data.title }}, etc, to our njks templates
+    for (let i = 0; i < coll.length; i++) {
+      const prevProject = coll[i - 1];
+      const nextProject = coll[i + 1];
+
+      coll[i].data["prevProject"] = prevProject;
+      coll[i].data["nextProject"] = nextProject;
+    }
+
+    return coll;
+  });
+
   eleventyConfig.addCollection("frontpage", function (collection) {
 		return collection.getFilteredByGlob(["src/posts/*.md", "src/notes/*.md"]).reverse();
   });
---
+++ src/projects/home-server.md
@@ -0,0 +1,12 @@
+---
+date: 2025-06-24
+title: Test Project
+description: This is a test of the project page on my blog.
+tags:
+  - projects
+layout: layouts/post.njk
+permalink: "{{page.filePathStem}}/"
+---
+# TEST PROJECT
+
+this is a test of the project page of my blog. is it working?
