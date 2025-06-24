e942e3d424eb3f2aa86c3f9b444dd7a9c2987e67
Robert Young <thegreekgeekseter@gmail.com>
2025-06-24 21:54:54 +0000

    Reversed the sort hopefully.

--- .eleventy.js
+++ .eleventy.js
@@ -125,7 +125,7 @@ module.exports = async function (eleventyConfig) {
   });
 
   eleventyConfig.addCollection("frontpage", function (collection) {
-		return collection.getFilteredByGlob(["src/posts/*.md", "src/notes/*.md"]);
+		return collection.getFilteredByGlob(["src/posts/*.md", "src/notes/*.md"]).reverse();
   });
 
   return {
